bfdec21e7532b6feee7c332ee7866a34e4ea4957
reacheight <reacheight8@gmail.com>
2017-12-27 00:02:09 +0300

    rewrited structure of program

--- 12/2/main.cpp
+++ 12/2/main.cpp
@@ -6,20 +6,8 @@
 
 using namespace std;
 
-int main()
+vector<vector<int>> readGraph(int n, ifstream& in)
 {
-    setlocale(LC_ALL, "Russian");
-
-    ifstream in("input.txt");
-    if (!in.is_open())
-    {
-        cout << "Не удалось открыть файл." << endl;
-        return 1;
-    }
-
-    int n = 0;
-    in >> n;
-
     vector<vector<int>> graph(n);
 
     for (int i = 0; i < n; ++i)
@@ -38,15 +26,17 @@ int main()
         }
     }
 
-    in.close();
+    return graph;
+}
 
-    int mstLen = 0;
+pair<int, vector<vector<int>>> getMinSpanningTree(vector<vector<int>>& graph, int n)
+{
+    int treeLength = 0;
+    vector<vector<int>> treeEdges;
     vector<bool> isUsed(n);
     vector<int> minLength(n, INT_MAX);
     vector<int> destination(n, -1);
     minLength[0] = 0;
-
-    cout << "Ребра минимального остовного дерева: " << endl;
 
     for (int i = 0; i < n; ++i) {
         int v = -1;
@@ -61,8 +51,8 @@ int main()
 
         if (destination[v] != -1)
         {
-            mstLen += minLength[v];
-            cout << "(" << v << " " << destination[v] << ")" << " ";
+            treeLength += minLength[v];
+            treeEdges.push_back({v, destination[v]});
         }
 
         for (int to = 0; to < n; ++to)
@@ -75,7 +65,40 @@ int main()
         }
     }
 
-    cout << endl << "Длина минимального оставного дерева: " << mstLen << endl;
+    return {treeLength, treeEdges};
+};
+
+void printAnswer(pair<int, vector<vector<int>>>& lengthAndEdges)
+{
+    cout << "Рёбра минимального остовного дерева: " << endl;
+    for (auto edge : lengthAndEdges.second)
+    {
+        cout << "(" << edge[0] << " " << edge[1] << ")" << " ";
+    }
+
+    cout << endl << "Длина минимального остовного дерева: " << lengthAndEdges.first << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    ifstream in("input.txt");
+    if (!in.is_open())
+    {
+        cout << "Не удалось открыть файл." << endl;
+        return 1;
+    }
+
+    int n = 0;
+    in >> n;
+
+    auto graph = readGraph(n, in);
+
+    in.close();
+
+    auto lengthAndEdges = getMinSpanningTree(graph, n);
+    printAnswer(lengthAndEdges);
 
     return 0;
 }
