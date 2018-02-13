4622a00287e010ada0f0f1d660bc50e3e8c0110a
reacheight <reacheight8@gmail.com>
2018-02-13 12:42:37 +0300

    uploaded homework 1.1

---
+++ second-semester/hw1-1/hw1-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace hw1_1
+{
+    class Program
+    {
+        static int Factorial(int n) => n <= 1 ? 1 : n * Factorial(n - 1);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое неотрицательное число:");
+            var inputData = Console.ReadLine();
+            int n;
+            while (!int.TryParse(inputData, out n) || n < 0)
+            {
+                if (n < 0)
+                {
+                    Console.WriteLine("Вы ввели отрицательное целое число.\nПопробуйте ещё раз:");
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели не целое число.\nПопробуйте ещё раз:");
+                }
+
+                inputData = Console.ReadLine();
+            }
+
+            Console.WriteLine("{0}! = {1}", n, Factorial(n));
+        }
+    }
+}
