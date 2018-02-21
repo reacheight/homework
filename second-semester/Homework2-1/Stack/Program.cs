c2cfb8959d2e86224eaad12a6bb4d41b028ba76d
reacheight <reacheight8@gmail.com>
2018-02-21 13:39:13 +0300

    uploaded 1st version of the stack implementation

---
+++ second-semester/Homework2-1/Stack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Stack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(3);
+            stack.Push(7);
+
+            Console.WriteLine(stack.Size());
+            Console.WriteLine(stack.Pop());
+
+            if (!stack.IsEmpty())
+            {
+                Console.WriteLine(stack.Top());
+            }
+        }
+    }
+}
