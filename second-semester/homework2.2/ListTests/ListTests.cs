e2f32e0c733af66a093be3f9913c850496bbb473
reacheight <reacheight8@gmail.com>
2018-02-27 11:15:21 +0300

    created project for unit-test and added test for 'IsEmpty' method

---
+++ second-semester/homework2.2/ListTests/ListTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using List;
+
+namespace List.Tests
+{
+    [TestClass()]
+    public class ListTests
+    {
+        private List<int> list;
+
+        [TestInitialize]
+        public void Init()
+        {
+            list = new List<int>();
+        }
+
+        [TestMethod()]
+        public void IsEmptyReturnsTrueIfListIsEmpty()
+        {
+            Assert.AreEqual(true, list.IsEmpty());
+        }
+
+        [TestMethod()]
+        public void InsertTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EraseTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValueTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
