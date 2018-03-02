2a5e084968c69991359afdeb854f22a9a706974c
reacheight <reacheight8@gmail.com>
2018-03-02 18:15:05 +0300

    added some examples of usage

--- second-semester/homework2.3/HashTable/Program.cs
+++ second-semester/homework2.3/HashTable/Program.cs
@@ -6,11 +6,32 @@ namespace HashTable
     {
         private static void Main(string[] args)
         {
-            var table = new HashTable();
+            var integerTable = new HashTable<int>();
 
-            table.Add("one");
-            table.Add("one");
+            for (var i = 1; i <= 100; ++i)
+            {
+                if (i % 2 == 1)
+                {
+                    integerTable.Add(i);
+                }
+            }
 
+            for (var i = 1; i <= 100; ++i)
+            {
+                if (i % 4 == 0 && integerTable.Contains(i))
+                {
+                    integerTable.Erase(i);
+                }
+            }
+
+            var stringTable = new HashTable<string>();
+
+            for (var i = 1; i <= 100; ++i)
+            {
+                stringTable.Add("word" + i.ToString());
+            }
+
+            Console.WriteLine("tralala");
             Console.Read();
         }
     }
