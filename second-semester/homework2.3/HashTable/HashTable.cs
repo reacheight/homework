975f6894044e12e7b34875dc8d190f11fac03b82
reacheight <reacheight8@gmail.com>
2018-03-02 17:46:40 +0300

    added ability to create hash table with elements of any type

--- second-semester/homework2.3/HashTable/HashTable.cs
+++ second-semester/homework2.3/HashTable/HashTable.cs
@@ -5,7 +5,8 @@ namespace HashTable
     /// <summary>
     /// Class that implement the hash-table
     /// </summary>
-    public class HashTable
+    /// <typeparam name="T">Type of the elements in hashtable</typeparam>
+    public class HashTable<T>
     {
         /// <summary>
         /// Number of lists in the hash table
@@ -15,14 +16,14 @@ namespace HashTable
         /// <summary>
         /// Array of the lists
         /// </summary>
-        private List<string>[] array;
+        private List<T>[] array;
 
         public HashTable()
         {
-            this.array = new List<string>[Size];
+            this.array = new List<T>[Size];
             for (int i = 0; i < Size; ++i)
             {
-                this.array[i] = new List<string>();
+                this.array[i] = new List<T>();
             }
         }
 
@@ -30,7 +31,7 @@ namespace HashTable
         /// Add value to the hash table
         /// </summary>
         /// <param name="value">Value added to the hash table</param>
-        public void Add(string value)
+        public void Add(T value)
         {
             if (!this.Contains(value))
             {
@@ -42,16 +43,17 @@ namespace HashTable
         /// Erase value from the hash table
         /// </summary>
         /// <param name="value">Value erased from the hash table</param>
-        public void Erase(string value)
+        public void Erase(T value)
         {
-            var position = this.Position(value);
+            var list = this.GetList(value);
+            var position = list.Position(value);
 
             if (position == -1)
             {
                 throw new ArgumentException("Попытка удалить элемент, которого нет в хэш-таблице");
             }
 
-            this.GetList(value).Erase(position);
+            list.Erase(position);
         }
 
         /// <summary>
@@ -59,7 +61,7 @@ namespace HashTable
         /// </summary>
         /// <param name="value">Verified value</param>
         /// <returns>True if value in the hash table, false otherwise</returns>
-        public bool Contains(string value) => this.Position(value) != -1;
+        public bool Contains(T value) => this.GetList(value).Position(value) != -1;
 
         /// <summary>
         /// Hash-function
@@ -83,28 +85,15 @@ namespace HashTable
         /// </summary>
         /// <param name="value">Value whose position is returned</param>
         /// <returns>Position of the value if value in the hash table, -1 otherwise</returns>
-        private int Position(string value)
-        {
-            var list = this.GetList(value);
-            for (var i = 0; i < list.Size; ++i)
-            {
-                if (list.Value(i) == value)
-                {
-                    return i;
-                }
-            }
-
-            return -1;
-        }
 
         /// <summary>
         /// Get list that contains value
         /// </summary>
         /// <param name="value">Value whose list it returned</param>
         /// <returns>List of the value</returns>
-        private List<string> GetList(string value)
+        private List<T> GetList(T value)
         {
-            int hash = HashFunc(value);
+            var hash = Math.Abs(value.GetHashCode());
             return this.array[hash % Size];
         }
     }
