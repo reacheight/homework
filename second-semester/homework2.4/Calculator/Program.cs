3548a00da6fb6510dae79b81f276fef9b8b5781f
reacheight <reacheight8@gmail.com>
2018-03-05 15:22:35 +0300

    uploaded example of usage

---
+++ second-semester/homework2.4/Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            var calculator = new Calculator(new ReferenceStack<double>());
+            for (int i = 1; i < 11; ++i)
+            {
+                calculator.Push(i);
+            }
+
+            while (calculator.Size >= 2)
+            {
+                calculator.Add();
+                Console.WriteLine(calculator.Result);
+            }
+
+            Console.Read();
+        }
+    }
+}
