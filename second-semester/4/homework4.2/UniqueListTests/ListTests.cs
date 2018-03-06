96f5717fbbe3266dc1ef261207d0cfe5fc78559c
reacheight <reacheight8@gmail.com>
2018-03-06 19:18:30 +0300

    added tests for list and unique list

---
+++ second-semester/4/homework4.2/UniqueListTests/ListTests.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UniqueList.Tests
+{
+    [TestClass()]
+    public class ListTests
+    {
+        private List<int> list;
+
+        [TestInitialize]
+        public void Init()
+        {
+            list = new List<int>();
+        }
+
+        [TestMethod]
+        public void IsEmptyReturnsTrueIfListIsEmpty()
+        {
+            Assert.IsTrue(list.IsEmpty());
+        }
+
+        [TestMethod]
+        public void InsertInHeadWillNotCrash()
+        {
+            list.Insert(56, 0);
+        }
+
+        [TestMethod]
+        public void InsertRandomIntegerInHeadWillNotCrash()
+        {
+            int randomInteger = new Random().Next();
+            list.Insert(randomInteger, 0);
+        }
+
+        [TestMethod]
+        public void InsertNotOnlyInHeadWillNotCrash()
+        {
+            var numberOfElements = 100;
+
+            for (int i = 0; i < numberOfElements; ++i)
+            {
+                list.Insert(i, i);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InsertInIncorrectPositionWillThrowExpectedException()
+        {
+            var numberOfElements = 100;
+            for (var i = 0; i < numberOfElements; ++i)
+            {
+                list.Insert(i, i);
+            }
+
+            list.Insert(40, numberOfElements + 10);
+        }
+
+        [TestMethod]
+        public void SizeOfEmptyListIsZero()
+        {
+            Assert.AreEqual(0, list.Size);
+        }
+
+        [TestMethod]
+        public void EraseHeadFromNonemptyListWillNotCrash()
+        {
+            list.Insert(14, 0);
+            list.Erase(0);
+        }
+
+        [TestMethod]
+        public void EraseNotOnlyFromHeadWillNotCrash()
+        {
+            var numberOfElements = 100;
+            for (var i = 0; i < numberOfElements; ++i)
+            {
+                list.Insert(i, i);
+            }
+
+            for (var i = numberOfElements - 1; i >= 0; --i)
+            {
+                list.Erase(i);
+            }
+        }
+
+        [TestMethod]
+        public void SizeReturnsRightValue()
+        {
+            var numberOfAddedElements = 100;
+            for (var i = 0; i < numberOfAddedElements; ++i)
+            {
+                list.Insert(i, i);
+            }
+
+            var numberOfRemovedElements = 50;
+            for (var i = numberOfRemovedElements - 1; i >= 0; --i)
+            {
+                list.Erase(i);
+            }
+
+            Assert.AreEqual(numberOfAddedElements - numberOfRemovedElements, list.Size);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ValueOfElementByIncorrectPositionWillThrowExpectedException()
+        {
+            var numberOfElements = 100;
+            for (var i = 0; i < numberOfElements; ++i)
+            {
+                list.Insert(i, i);
+            }
+
+            var value = list.Value(numberOfElements);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void EraseFromIncorrectPositionWillThrowExpectedException()
+        {
+            var numberOfElements = 100;
+            for (var i = 0; i < numberOfElements; ++i)
+            {
+                list.Insert(i, i);
+            }
+
+            list.Erase(numberOfElements);
+        }
+
+        [TestMethod]
+        public void ValueWorksRight()
+        {
+            var numberOfElements = 100;
+            var array = new int[numberOfElements];
+            for (var i = 0; i < numberOfElements; ++i)
+            {
+                array[i] = i;
+                list.Insert(i, i);
+            }
+
+            for (var i = 0; i < numberOfElements; ++i)
+            {
+                Assert.AreEqual(array[i], list.Value(i));
+            }
+        }
+
+        [TestMethod]
+        public void PositionWorksRight()
+        {
+            var numberOfElements = 100;
+            for (var i = 0; i < numberOfElements; ++i)
+            {
+                list.Insert(i, i);
+            }
+
+            for (var i = 0; i < numberOfElements; ++i)
+            {
+                Assert.AreEqual(i, list.Position(i));
+            }
+
+            for (var i = numberOfElements; i < 2 * numberOfElements; ++i)
+            {
+                Assert.AreEqual(-1, list.Position(i));
+            }
+        }
+    }
+}
