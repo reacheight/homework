ff3150ff897a75f26a140ba2f52b7c083c6b5b79
reacheight <reacheight8@gmail.com>
2018-03-06 19:16:01 +0300

    uploaded list and unique list classes

---
+++ second-semester/4/homework4.2/UniqueList/UniqueList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UniqueList
+{
+    public class UniqueList<T> : List<T>
+    {
+        public new void Insert(T value, int position)
+        {
+            if (this.Position(value) != -1)
+            {
+                throw new ValueAlreadyInListException("Попытка добавить в список значение, которое уже находится в списке.");
+            }
+
+            base.Insert(value, position);
+        }
+
+        public void EraseValue(T value)
+        {
+            var position = this.Position(value);
+
+            if (position == -1)
+            {
+                throw new ValueNotInListException("Попытка удалить из списка значение, которого нет в списке.");
+            }
+
+            this.Erase(position);
+        }
+    }
+}
