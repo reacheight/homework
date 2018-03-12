70b8d26c10432827f87334a558d5ce8956750be5
reacheight <reacheight8@gmail.com>
2018-03-12 15:57:41 +0300

    uploaded example of usage hash table

---
+++ second-semester/homework3.2/HashFunction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HashTable
+{
+    public class Program
+    {
+        public static int IntHashFunction(int value) => value + 7;
+
+        public static int StringHashFunction(string value) => value.Length;
+
+        private static void Main(string[] args)
+        {
+            var table = new HashTable<char>();
+            var intTable = new HashTable<int>(IntHashFunction);
+            var stringTable = new HashTable<string>(StringHashFunction);
+
+            for (var i = 0; i < 50; ++i)
+            {
+                intTable.Add(i);
+                stringTable.Add(i.ToString());
+                table.Add(i.ToString()[0]);
+            }
+
+            try
+            {
+                intTable.Erase(183);
+            }
+            catch (ValueNotInHashTableException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
