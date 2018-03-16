cd6b7e201078a673982301afaa71c32939e7cf06
reacheight <reacheight8@gmail.com>
2018-03-16 21:00:16 +0300

    updated example of usage

--- second-semester/5/ListFunctions/ListFunctions/Program.cs
+++ second-semester/5/ListFunctions/ListFunctions/Program.cs
@@ -1,30 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ListFunctions
+﻿namespace ListFunctions
 {
+    using System;
+    using System.Collections.Generic;
+
     public class Program
     {
         private static void Main(string[] args)
         {
             var list = new List<int> { 1, 2, 3, 4 };
-            var list1 = ListFunctions.Map<int, int>(list, x => x + 1);
+            var list1 = ListFunctions.Map(list, x => x + 1);
             var list2 = ListFunctions.Filter(list, x => x % 2 == 0);
-            var list3 = ListFunctions.Fold<int, int>(list, 1, (acc, next) => acc * next);
+            var result = ListFunctions.Fold(list, 1, (acc, next) => acc * next);
 
             foreach (var item in list1)
             {
                 Console.Write($"{item} ");
             }
+
             Console.WriteLine();
 
             foreach (var item in list2)
             {
                 Console.Write($"{item} ");
             }
+
             Console.WriteLine();
 
-            Console.WriteLine(list3);
+            Console.WriteLine(result);
         }
     }
 }
