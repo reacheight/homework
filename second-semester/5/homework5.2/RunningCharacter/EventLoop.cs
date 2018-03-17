e3a9b4698267a6f1a215e9d4ae4058f2370502a8
reacheight <reacheight8@gmail.com>
2018-03-17 16:51:01 +0300

    added EventLoop class

---
+++ second-semester/5/homework5.2/RunningCharacter/EventLoop.cs
@@ -0,0 +1,45 @@
+﻿namespace RunningCharacter
+{
+    using System;
+
+    public class EventLoop
+    {
+        public event EventHandler<EventArgs> LeftHandler = (sender, args) => { };
+
+        public event EventHandler<EventArgs> RightHandler = (sender, args) => { };
+
+        public event EventHandler<EventArgs> UpHandler = (sender, args) => { };
+
+        public event EventHandler<EventArgs> DownHandler = (sender, args) => { };
+
+        public event EventHandler<EventArgs> StartHandler = (sender, args) => { };
+
+        public void Run()
+        {
+            this.StartHandler(this, EventArgs.Empty);
+
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        this.LeftHandler(this, EventArgs.Empty);
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        this.RightHandler(this, EventArgs.Empty);
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        this.DownHandler(this, EventArgs.Empty);
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        this.UpHandler(this, EventArgs.Empty);
+                        break;
+                }
+            }
+        }
+    }
+}
