e9cb7860636add5346bb17ae22f5b62b01af88b1
reacheight <reacheight8@gmail.com>
2018-03-24 17:06:36 +0300

    added documentation for operand class

--- second-semester/4/homework4.1/ExpressionTree/Operand.cs
+++ second-semester/4/homework4.1/ExpressionTree/Operand.cs
@@ -2,17 +2,36 @@
 {
     using System;
 
+    /// <summary>
+    /// Class that implements real operand
+    /// </summary>
     public class Operand : Node
     {
+        /// <summary>
+        /// Real value of the operand
+        /// </summary>
         private double value;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Operand"/> class.
+        /// </summary>
+        /// <param name="value">Real value of the operand</param>
         public Operand(double value)
         {
             this.value = value;
         }
 
+        /// <summary>
+        /// Gets infix notation of a parse tree with the root in that node
+        /// In this case it is the value itself
+        /// </summary>
         public override string InfixNotation => this.value.ToString();
 
+        /// <summary>
+        /// Evaluate value of arithmetic expression of a parse tree with the root in that node
+        /// In this case it is the value itself
+        /// </summary>
+        /// <returns>Value of arithmetic expression of a parse tree with the root in that node</returns>
         public override double Value() => this.value;
     }
 }
