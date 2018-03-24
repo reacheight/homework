47b88bd6bf84a86d28ccabf75ab2f5470f3cc3c1
reacheight <reacheight8@gmail.com>
2018-03-24 17:17:32 +0300

    added documentation for all operation classes

--- second-semester/4/homework4.1/ExpressionTree/DivisionOperator.cs
+++ second-semester/4/homework4.1/ExpressionTree/DivisionOperator.cs
@@ -2,19 +2,34 @@
 {
     using System;
 
+    /// <summary>
+    /// Class that represent division operator node of a tree
+    /// </summary>
     public class DivisionOperator : Operator
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DivisionOperator"/> class.
+        /// </summary>
         public DivisionOperator()
         {
             this.OperatorChar = '/';
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DivisionOperator"/> class.
+        /// </summary>
+        /// <param name="leftChild">Left node's child</param>
+        /// <param name="rightChild">Right node's child</param>
         public DivisionOperator(Node leftChild, Node rightChild)
             : base(leftChild, rightChild)
         {
             this.OperatorChar = '/';
         }
 
+        /// <summary>
+        /// Evaluate value of arithmetic expression of a parse tree with the root in that node
+        /// </summary>
+        /// <returns>Value of arithmetic expression of a parse tree with the root in that node</returns>
         public override double Value()
         {
             if (this.RightChild.Value() < Math.Abs(0.000001))
--- second-semester/4/homework4.1/ExpressionTree/SubtractionOperator.cs
+++ second-semester/4/homework4.1/ExpressionTree/SubtractionOperator.cs
@@ -2,19 +2,34 @@
 {
     using System;
 
+    /// <summary>
+    /// Class that represent subtraction operator node of a tree
+    /// </summary>
     public class SubtractionOperator : Operator
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SubtractionOperator"/> class.
+        /// </summary>
         public SubtractionOperator()
         {
             this.OperatorChar = '-';
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SubtractionOperator"/> class.
+        /// </summary>
+        /// <param name="leftChild">Left node's child</param>
+        /// <param name="rightChild">Right node's child</param>
         public SubtractionOperator(Node leftChild, Node rightChild)
             : base(leftChild, rightChild)
         {
             this.OperatorChar = '-';
         }
 
+        /// <summary>
+        /// Evaluate value of arithmetic expression of a parse tree with the root in that node
+        /// </summary>
+        /// <returns>Value of arithmetic expression of a parse tree with the root in that node</returns>
         public override double Value() => this.LeftChild.Value() - this.RightChild.Value();
     }
 }
