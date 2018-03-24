075b676c9e628f915ac6c4afdc21c6546536dcd8
reacheight <reacheight8@gmail.com>
2018-03-24 17:10:53 +0300

    added documentation for operator class

--- second-semester/4/homework4.1/ExpressionTree/Operator.cs
+++ second-semester/4/homework4.1/ExpressionTree/Operator.cs
@@ -2,19 +2,36 @@
 {
     using System;
 
+    /// <summary>
+    /// Class that implements operator node of a tree
+    /// </summary>
     public abstract class Operator : Node
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Operator"/> class.
+        /// </summary>
         public Operator()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Operator"/> class.
+        /// </summary>
+        /// <param name="leftChild">Left node's child</param>
+        /// <param name="rightChild">Right node's child</param>
         public Operator(Node leftChild, Node rightChild)
             : base(leftChild, rightChild)
         {
         }
 
+        /// <summary>
+        /// Gets or sets operation character
+        /// </summary>
         public char OperatorChar { get; set; }
 
+        /// <summary>
+        /// Gets infix notation of a parse tree with the root in that node
+        /// </summary>
         public override string InfixNotation =>
             $"({this.LeftChild.InfixNotation} {this.OperatorChar} {this.RightChild.InfixNotation})";
     }
