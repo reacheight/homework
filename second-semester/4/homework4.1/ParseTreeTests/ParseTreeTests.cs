862e2a22816784a69caadbc6eaa41e0f2c500cf3
reacheight <reacheight8@gmail.com>
2018-03-24 18:59:06 +0300

    added some tests for parse tree

---
+++ second-semester/4/homework4.1/ParseTreeTests/ParseTreeTests.cs
@@ -0,0 +1,43 @@
+﻿namespace ExpressionTree.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+
+    [TestClass]
+    public class ParseTreeTests
+    {
+        private const double Delta = 0.000001;
+        private Dictionary<string, (double value, string infixNotation)> table = new Dictionary<string, (double value, string infixNotation)>
+        {
+            ["(+ 2 3)"] = (5, "(2 + 3)"),
+            ["(- 10 4)"] = (6, "(10 - 4)"),
+            ["(* 3 4)"] = (12, "(3 * 4)"),
+            ["(/ 10 4)"] = (2.5, "(10 / 4)"),
+            ["(+ (* 3 4) (- 10 5))"] = (17, "((3 * 4) + (10 - 5))"),
+            ["(- (/ 20 4) (+ 10.5 5))"] = (-10.5, "((20 / 4) - (10,5 + 5))"),
+            ["(* (+ (- 15 (* 2 5)) 6) (+ (- (* 4 1) 3) (+ (/ 7 (+ 3 4)) 1)))"] =
+            (33, "(((15 - (2 * 5)) + 6) * (((4 * 1) - 3) + ((7 / (3 + 4)) + 1)))"),
+        };
+
+        [TestMethod]
+        public void TestTreeValue()
+        {
+            foreach(var key in this.table.Keys)
+            {
+                var tree = new ParseTree(key);
+                Assert.AreEqual(table[key].value, tree.Value, Delta);
+            }
+        }
+
+        [TestMethod]
+        public void TestTreeInfixNotation()
+        {
+            foreach (var key in this.table.Keys)
+            {
+                var tree = new ParseTree(key);
+                Assert.AreEqual(table[key].infixNotation, tree.InfixNotation);
+            }
+        }
+    }
+}
