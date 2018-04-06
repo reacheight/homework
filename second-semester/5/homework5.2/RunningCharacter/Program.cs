5ec3bf83bff924c32ee16e1c7058ebcfe8a661f4
reacheight <reacheight8@gmail.com>
2018-04-06 10:15:16 +0300

    added documentation for main class of the program

--- second-semester/5/homework5.2/RunningCharacter/Program.cs
+++ second-semester/5/homework5.2/RunningCharacter/Program.cs
@@ -1,11 +1,18 @@
 ﻿namespace RunningCharacter
 {
-    using System;
     using System.Collections.Generic;
     using System.IO;
 
+    /// <summary>
+    /// Main class of the program
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Reads map from the text file
+        /// </summary>
+        /// <param name="filename">Name of the text file</param>
+        /// <returns>Map as list of strings</returns>
         private static List<string> ReadMap(string filename)
         {
             var result = new List<string>();
@@ -17,6 +24,9 @@
             return result;
         }
 
+        /// <summary>
+        /// Main method of the program
+        /// </summary>
         private static void Main(string[] args)
         {
             var map = ReadMap("map.txt");
