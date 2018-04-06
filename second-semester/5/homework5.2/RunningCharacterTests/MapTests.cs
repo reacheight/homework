1a4fe27be3d6158bbb2394ad83bf02e69dfb4dd4
reacheight <reacheight8@gmail.com>
2018-04-06 11:27:20 +0300

    added test for wrong and valids maps

---
+++ second-semester/5/homework5.2/RunningCharacterTests/MapTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RunningCharacter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunningCharacter.Tests
+{
+    [TestClass]
+    public class MapTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(WrongMapException))]
+        public void FirstWrongMapThrowExprectedException()
+        {
+            var map = new Map("WrongMaps/map1.txt");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(WrongMapException))]
+        public void SecondWrongMapThrowExprectedException()
+        {
+            var map = new Map("WrongMaps/map3.txt");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(WrongMapException))]
+        public void EmptyMapThrowExprectedException()
+        {
+            var map = new Map("WrongMaps/map2.txt");
+        }
+
+        [TestMethod]
+        public void ValidMapsWillNotCrash()
+        {
+            var validMaps = new List<string>() {
+                "validMaps/map1.txt",
+                "validMaps/map2.txt",
+                "validMaps/map3.txt",
+                "validMaps/map4.txt",
+                "validMaps/map5.txt",
+            };
+
+            foreach (var filename in validMaps)
+            {
+                var map = new Map(filename);
+            }
+        }
+    }
+}
