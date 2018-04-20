2a86734bc5db1a4e39bc42f0290c767c29879abe
reacheight <reacheight8@gmail.com>
2018-04-20 09:44:00 +0300

    all digit button click handlers replaced by one handler

--- second-semester/6/homework6.1/Calculator/Form1.cs
+++ second-semester/6/homework6.1/Calculator/Form1.cs
@@ -40,54 +40,10 @@ namespace Calculator
 
         Calculator calculator = new Calculator();
 
-        private void button1_Click(object sender, EventArgs e)
+        private void DigitButtonClick(object sender, EventArgs e)
         {
-            this.textBox.Text += "1";
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            this.textBox.Text += "2";
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            this.textBox.Text += "3";
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            this.textBox.Text += "4";
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            this.textBox.Text += "5";
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            this.textBox.Text += "6";
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            this.textBox.Text += "7";
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            this.textBox.Text += "8";
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            this.textBox.Text += "9";
-        }
-
-        private void button0_Click(object sender, EventArgs e)
-        {
-            this.textBox.Text += "0";
+            var button = sender as Button;
+            this.textBox.Text += button.Text;
         }
 
         private void buttonComma_Click(object sender, EventArgs e)
