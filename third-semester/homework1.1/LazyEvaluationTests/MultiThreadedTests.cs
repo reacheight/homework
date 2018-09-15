0d9b304a06d9aa14c5b7944306d812a8dda80509
reacheight <reacheight8@gmail.com>
2018-09-15 14:32:20 +0400

    removed reduntan using lines

--- third-semester/homework1.1/LazyEvaluationTests/MultiThreadedTests.cs
+++ third-semester/homework1.1/LazyEvaluationTests/MultiThreadedTests.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Threading;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace LazyEvaluation.Tests
 {
