b7006c4e305d1939c0dc6752adad196c05dcb464
reacheight <reacheight8@gmail.com>
2018-09-27 15:56:20 +0300

    implemented MyThreadPool

---
+++ third-semester/homework2/MyThreadPool/IMyTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyThreadPool
+{
+    public interface IMyTask<TResult>
+    {
+        bool IsCompleted { get; }
+        TResult Result { get; }
+
+        IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> newTask);
+    }
+}
