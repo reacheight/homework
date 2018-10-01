2c32e11edb4c233b58bc2935b512f2c5241c0012
reacheight <reacheight8@gmail.com>
2018-10-01 14:22:16 +0300

    added example of usage

--- third-semester/homework2/MyThreadPoolAndTask/Program.cs
+++ third-semester/homework2/MyThreadPoolAndTask/Program.cs
@@ -1,26 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace MyThreadPoolAndTask
 {
     public class Program
     {
-        private static void Main(string[] args)
+        public static int Factorial(int n)
         {
-            var threadPool = new MyThreadPool(4);
+            if (n < 0) throw new ArgumentOutOfRangeException();
 
-            var count = 0;
+            if (n <= 1) return 1;
 
-            var task = threadPool.QueueTask(() =>
+            return Factorial(n - 1) * n;
+        }
+        
+        public static void Main(string[] args)
+        {
+            var threadPool = new MyThreadPool(5);
+            var tasks = new List<IMyTask<int>>();
+            for (var i = 0; i < 10; ++i)
             {
-                count++;
-                Thread.Sleep(5000);
-                return 5;
+                var j = i;
+                tasks.Add(threadPool.QueueTask(() => Factorial(j)));
+            }
+
+            Thread.Sleep(1000);
+
+            foreach (var task in tasks)
+            {
+                Console.WriteLine(task.Result);
+            }
+
+            tasks[0].ContinueWith((x) =>
+            {
+                Console.WriteLine("New task is evaluating!");
+                return x * x;
             });
-
-            Thread.Sleep(6000);
-
-            Console.WriteLine(count);
+            
             threadPool.Shutdown();
         }
     }
