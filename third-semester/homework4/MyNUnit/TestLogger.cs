eb43490e1d1f6345751e62dc206af4530cc0cf08
reacheight <reacheight8@gmail.com>
2018-11-08 13:09:16 +0300

    added documentation

--- third-semester/homework4/MyNUnit/TestLogger.cs
+++ third-semester/homework4/MyNUnit/TestLogger.cs
@@ -3,18 +3,42 @@ using System.Reflection;
 
 namespace MyNUnit
 {
+    /// <summary>
+    /// Class that implements logger for test system
+    /// </summary>
     public static class TestLogger
     {
+        /// <summary>
+        /// Gets full name of given method
+        /// </summary>
+        /// <param name="methodInfo">method info of given method</param>
+        /// <returns>full name of given method</returns>
         private static string MethodString(MethodInfo methodInfo)
             => $"{methodInfo.DeclaringType}.{methodInfo.Name}";
         
+        /// <summary>
+        /// Logs result of successful test execution
+        /// </summary>
+        /// <param name="methodInfo">method info of executed test mehtod</param>
+        /// <param name="ellapsedMilliseconds">test method execution time in milliseconds</param>
         public static void LogSuccess(MethodInfo methodInfo, long ellapsedMilliseconds)
             => Log($"Test method {MethodString(methodInfo)} successed.", ellapsedMilliseconds);
 
+        /// <summary>
+        /// Logs result of failed test execution
+        /// </summary>
+        /// <param name="methodInfo">method info of executed test</param>
+        /// <param name="ellapsedMilliseconds">test method execution time in milliseconds</param>
+        /// <param name="exception">exception thrown by test method</param>
         public static void LogFail(MethodInfo methodInfo, long ellapsedMilliseconds, Exception exception)
             => Log($"Test method {MethodString(methodInfo)} failed with exception message: {exception.Message}",
                 ellapsedMilliseconds);
 
+        /// <summary>
+        /// Prints given message and execution time
+        /// </summary>
+        /// <param name="message">given message</param>
+        /// <param name="ellapsedMilliseconds">test method execution time in milliseconds</param>
         private static void Log(string message, long ellapsedMilliseconds)
         {
             Console.WriteLine(message);
--- third-semester/homework4/MyNUnit/TestSystem.cs
+++ third-semester/homework4/MyNUnit/TestSystem.cs
@@ -7,8 +7,15 @@ using System.Threading.Tasks;
 
 namespace MyNUnit
 {
+    /// <summary>
+    /// Class that implements test system
+    /// </summary>
     public static class TestSystem
     {
+        /// <summary>
+        /// Executes tests in all classes of all assemblies stored by given path
+        /// </summary>
+        /// <param name="path">given path</param>
         public static void RunTests(string path)
         {
             var assemblies = Directory.GetFiles(path, "*.dll", SearchOption.AllDirectories)
@@ -22,6 +29,10 @@ namespace MyNUnit
             Task.WaitAll(tasks.ToArray());
         }
 
+        /// <summary>
+        /// Executes tests in given class
+        /// </summary>
+        /// <param name="type">type of given class</param>
         private static void RunTestMethods(Type type)
         {
             RunAttributeMethods<BeforeClassAttribute>(type);
@@ -29,6 +40,11 @@ namespace MyNUnit
             RunAttributeMethods<AfterClassAttribute>(type);
         }
         
+        /// <summary>
+        /// Executes all methods of given class with given attribute in parallel
+        /// </summary>
+        /// <param name="type">type of class whose methods are going to be executed</param>
+        /// <typeparam name="T">given attribute</typeparam>
         private static void RunAttributeMethods<T>(Type type)
             where T : Attribute
         {
@@ -49,6 +65,10 @@ namespace MyNUnit
                     .Any(attr => attr.GetType() == typeof(T));
         }
 
+        /// <summary>
+        /// Executes test method 
+        /// </summary>
+        /// <param name="methodInfo">method info of test method to be executed</param>
         private static void RunTestMethod(MethodInfo methodInfo)
         {
             ValidateMethod(methodInfo);
@@ -71,6 +91,10 @@ namespace MyNUnit
             RunAttributeMethods<AfterAttribute>(methodInfo.DeclaringType);
         }
 
+        /// <summary>
+        /// Executes simple method
+        /// </summary>
+        /// <param name="methodInfo">method info of method to be executed</param>
         private static void RunHelpMethod(MethodInfo methodInfo)
         {
             ValidateMethod(methodInfo);
@@ -78,6 +102,12 @@ namespace MyNUnit
             methodInfo.Invoke(instance, null);
         }
 
+        /// <summary>
+        /// Validates that given method is executable
+        /// </summary>
+        /// <param name="methodInfo">method info of method to be validated</param>
+        /// <exception cref="Exception">throws exception if method has any parameters
+        /// or is not void</exception>
         private static void ValidateMethod(MethodInfo methodInfo)
         {
             if (methodInfo.GetParameters().Length != 0)
@@ -91,6 +121,13 @@ namespace MyNUnit
             }
         }
 
+        /// <summary>
+        /// Creates instance of given type
+        /// </summary>
+        /// <param name="type">given type</param>
+        /// <returns>instance of given type</returns>
+        /// <exception cref="Exception">throws exception if given type
+        /// has no parameterless constructor</exception>
         private static object CreateInstance(Type type)
         {
             var constructor = type.GetConstructor(Type.EmptyTypes);
