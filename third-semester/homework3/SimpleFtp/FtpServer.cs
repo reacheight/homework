f20ba4f01c0315a14cfed531eb8c6a0345d7d87a
reacheight <reacheight8@gmail.com>
2018-11-30 18:09:06 +0300

    added first version of ftp server

---
+++ third-semester/homework3/SimpleFtp/FtpServer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleFtp
+{
+    public class FtpServer
+    {
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        private readonly int _port;
+        private readonly int _maxConnectionNumber;
+        private int _currentConnectionNumber;
+
+        public FtpServer(int port, int maxConnectionNumber)
+        {
+            if (port < IPEndPoint.MinPort || port > IPEndPoint.MaxPort)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            
+            _port = port;
+            _maxConnectionNumber = maxConnectionNumber;
+        }
+
+        public async Task Start()
+        {
+            var listener = new TcpListener(IPAddress.Any, _port);
+            listener.Start();
+            
+            Console.WriteLine($"Server started to listen to port {_port}...");
+
+            while (!_cancellationTokenSource.IsCancellationRequested)
+            {
+                using (var client = await listener.AcceptTcpClientAsync())
+                {
+                    if (Interlocked.CompareExchange(ref _currentConnectionNumber, 0, 0) == _maxConnectionNumber)
+                    {
+                        client.Close();
+                        continue;
+                    }
+
+                    Interlocked.Increment(ref _currentConnectionNumber);
+                    Console.WriteLine("New client connected.");
+
+                    await Interact(client);
+                }
+
+                Interlocked.Decrement(ref _currentConnectionNumber);
+                Console.WriteLine("Client disconnected.");
+            }
+        }
+
+        private async Task Interact(TcpClient client)
+        {
+            var reader = new StreamReader(client.GetStream());
+            var writer = new StreamWriter(client.GetStream()) {AutoFlush = true};
+
+            var query = await reader.ReadLineAsync();
+            while (query != "dc")
+            {
+                var (command, path) = ParseQuery(query);
+                if (command == "1")
+                {
+                    await writer.WriteLineAsync(ListCommandResult(path));
+                }
+                
+                query = await reader.ReadLineAsync();
+            }
+        }
+        
+        public void Stop()
+        {
+            _cancellationTokenSource.Cancel();
+            _currentConnectionNumber = 0;
+        }
+
+        private string ListCommandResult(string path)
+        {
+            try
+            {
+                var files = Directory.GetFiles(path);
+                var dirictories = Directory.GetDirectories(path);
+                
+                return files.Length + dirictories.Length + " "
+                       + string.Join(" false ", files) + " false "
+                       + string.Join(" true ", dirictories) + " true";
+            }
+            catch (Exception)
+            {
+                return "-1";
+            }
+        }
+        
+        private (string, string) ParseQuery(string query)
+        {
+            var tokens = query.Split(' ');
+            if (tokens.Length != 2)
+            {
+                //TODO proper exception
+                throw new Exception();
+            }
+
+            return(tokens[0], tokens[1]);
+        }
+    }
+}
---
+++ third-semester/homework3/SimpleFtp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleFtp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var server = new FtpServer(12345, 5);
+            server.Start();
+
+            var client = new TcpClient("localhost", 12345);
+            var reader = new StreamReader(client.GetStream());
+            var writer = new StreamWriter(client.GetStream()) {AutoFlush = true};
+
+            var query = "1 .";
+            writer.WriteLine(query);
+            
+            var data = reader.ReadLine();
+            Console.WriteLine($"Server: {data}");
+
+            Console.Read();
+        }
+    }
+}
