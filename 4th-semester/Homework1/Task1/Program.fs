91c0320aaf6239939191efa623d4d40b3a0dec57
reacheight <reacheight8@gmail.com>
2019-02-15 23:07:31 +0300

    added solution for first task

---
+++ 4th-semester/Homework1/Task1/Program.fs
@@ -0,0 +1,23 @@
+﻿namespace Task1
+open System
+
+module Factorial =
+    let rec factorial x = 
+        let rec accFactorial n acc = 
+            match n with 
+            | 0 | 1 -> acc
+            | _ -> accFactorial (n - 1) (acc * n)
+        
+        if x < 0 then
+            raise (ArgumentOutOfRangeException())
+        else
+            accFactorial x 1
+        
+    Console.WriteLine("Enter an integer: ")
+    let input : int = Console.ReadLine() |> int
+    Console.WriteLine(if input < 0
+        then "You entered a negative integer"
+        else "Factorial: " + (factorial input |> string))
+        
+    
+    
