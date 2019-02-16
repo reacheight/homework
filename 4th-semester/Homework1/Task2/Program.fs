8fe8d7cb12abc0e2db08bdbfc9cdc68f29b73840
reacheight <reacheight8@gmail.com>
2019-02-16 16:44:27 +0300

    small changes

--- 4th-semester/Homework1/Task2/Program.fs
+++ 4th-semester/Homework1/Task2/Program.fs
@@ -1,17 +1,18 @@
 ﻿open System
 
-let rec fibonacci n = 
-    let rec accFibonacci n cur prev = 
-        match n with 
-        | 0 | 1 -> cur
-        | _ -> accFibonacci (n - 1) (cur + prev) cur
-       
-    if n < 0
-        then raise (ArgumentOutOfRangeException())
-        else accFibonacci n 1 1
-        
-Console.WriteLine("Enter an integer: ")
-let input : int = Console.ReadLine() |> int
-Console.WriteLine(if input < 0
-    then "You entered a negative integer"
-    else "Fibonacci: " + (input |> fibonacci |> string))
+module Fibonacci = 
+    let rec fibonacci n = 
+        let rec accFibonacci n cur prev = 
+            match n with 
+            | 0 | 1 -> cur
+            | _ -> accFibonacci (n - 1) (cur + prev) cur
+           
+        if n < 0
+            then raise (ArgumentOutOfRangeException())
+            else accFibonacci n 1 1
+            
+    Console.WriteLine("Enter an integer: ")
+    let input : int = Console.ReadLine() |> int
+    Console.WriteLine(if input < 0
+        then "You entered a negative integer"
+        else "Fibonacci: " + (input |> fibonacci |> string))
