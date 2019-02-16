d6d563b845cb80a8b13d09a5f9ebe1b86e2d463f
reacheight <reacheight8@gmail.com>
2019-02-16 17:03:00 +0300

    added solution for 3rd task

---
+++ 4th-semester/Homework1/Task3/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+module Reversion =
+    let rec reversion ls = 
+        let rec accReversion ls acc = 
+            match List.length ls with 
+            | 0 -> acc
+            | _ -> accReversion (List.tail ls) (List.head ls :: acc)
+           
+        accReversion ls []
+    
+    let ls = [4 .. 17] @ [3 .. 5] @ [153; 1235; -124]
+    printfn "%A" (reversion ls)
