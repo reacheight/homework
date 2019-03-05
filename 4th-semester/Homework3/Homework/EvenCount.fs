9d6c09a822a38b273a75d82160bc8e4d8b772906
reacheight <reacheight8@gmail.com>
2019-03-05 22:06:13 +0300

    added comments

--- 4th-semester/Homework3/Homework/EvenCount.fs
+++ 4th-semester/Homework3/Homework/EvenCount.fs
@@ -1,11 +1,15 @@
 namespace Homework
 
+/// Module for implementing functions that count amount of even numbers in a list
 module EvenCount =
+    /// Counts amount of even numbers in a list using Seq.filter
     let evenCountFirst list =
         list |> Seq.filter (fun x -> (abs x) % 2 = 0) |> Seq.length
     
+    /// Counts amount of even numbers in a list using Seq.map
     let evenCountSecond list =
         list |> Seq.map (fun x -> (abs x + 1) % 2) |> Seq.sum
     
+    /// Counts amount of even numbers in a list using Seq.fold
     let evenCountThird list =
         list |> Seq.fold (fun acc x -> acc + (abs x + 1) % 2) 0
--- 4th-semester/Homework3/Homework/PrimeNumbers.fs
+++ 4th-semester/Homework3/Homework/PrimeNumbers.fs
@@ -1,8 +1,11 @@
 namespace Homework
 
+/// Module for implementing infinite sequence of prime numbers
 module PrimeNumbers =
+    /// Checks whether number is prime
     let isPrime n =
             let sqrt' = n |> double |> sqrt |> int
             n > 1 && {2 .. sqrt'} |> Seq.forall (fun m -> n % m <> 0)
-            
+    
+    /// Infinite prime number sequence
     let primes = Seq.initInfinite int |> Seq.filter isPrime
