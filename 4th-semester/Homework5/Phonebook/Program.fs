23bb0e82e3ca65a5ae285d168669236568758730
reacheight <reacheight8@gmail.com>
2019-04-24 16:55:33 +0300

    implemented menu printing

---
+++ 4th-semester/Homework5/Phonebook/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let printMenu () =
+    printfn "1 - выход"
+    printfn "2 - добавить новую запись"
+    printfn "3 - найти телефон по имени"
+    printfn "4 - найти имя по телефону"
+    printfn "5 - вывести всё текущее содержимое справочника"
+    printfn "6 - сохранить текущие данные в файл"
+    printfn "7 - считать данные из файла"
+
+[<EntryPoint>]
+let main argv =
+    printMenu ()
+    0 // return an integer exit code
+
