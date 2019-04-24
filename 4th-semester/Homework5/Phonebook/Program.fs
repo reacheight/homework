30a90ab85ac766e08540e3955bd9bf3078e334f3
reacheight <reacheight8@gmail.com>
2019-04-24 17:38:02 +0300

    added print phonebook command

--- 4th-semester/Homework5/Phonebook/Program.fs
+++ 4th-semester/Homework5/Phonebook/Program.fs
@@ -9,23 +9,35 @@ let printHelp () =
     printfn "2 - добавить новую запись"
     printfn "3 - найти телефон по имени"
     printfn "4 - найти имя по телефону"
-    printfn "5 - вывести всё текущее содержимое справочника"
+    printfn "5 - напечатать всё текущее содержимое справочника"
     printfn "6 - сохранить текущие данные в файл"
     printfn "7 - считать данные из файла"
     
-let rec programLoop () =
+let printPhonebook (phonebook : Map<string, string>) =
+    let rec printPairList ls =
+        match ls with
+        | [] -> ()
+        | (key, value) :: tail -> printfn "\t%s : %s" key value; printPairList tail
+    
+    printfn "%s" <| if Map.isEmpty phonebook
+                        then "Справочник пуст."
+                        else "Все записи справочника:\n\tимя : номер"
+    printPairList (Map.toList phonebook)
+    phonebook
+    
+let rec programLoop (phonebook : Map<string, string>) =
     printf "Введите команду: "
     let input = Console.ReadLine()
     match input with
-    | "0" -> printHelp (); programLoop ()
+    | "0" -> printHelp (); programLoop phonebook
     | "1" -> ()
-    | _ -> printfn "not implemented"; programLoop ()
+    | "5" -> printPhonebook phonebook |> programLoop
+    | _ -> printfn "not implemented"; programLoop phonebook
 
 [<EntryPoint>]
 let main argv =
     printHelp ()
-    
-    programLoop ()
+    programLoop Map.empty
         
     0
 
