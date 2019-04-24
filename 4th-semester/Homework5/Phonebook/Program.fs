7e61b9fb2477d1935adc398c99eafa3c70523b26
reacheight <reacheight8@gmail.com>
2019-04-24 17:06:24 +0300

    added main loop with two commands

--- 4th-semester/Homework5/Phonebook/Program.fs
+++ 4th-semester/Homework5/Phonebook/Program.fs
@@ -2,7 +2,9 @@
 
 open System
 
-let printMenu () =
+let printHelp () =
+    printfn "Доступные команды: "
+    printfn "0 - напечатать список досутпных команд"
     printfn "1 - выход"
     printfn "2 - добавить новую запись"
     printfn "3 - найти телефон по имени"
@@ -10,9 +12,20 @@ let printMenu () =
     printfn "5 - вывести всё текущее содержимое справочника"
     printfn "6 - сохранить текущие данные в файл"
     printfn "7 - считать данные из файла"
+    
+let rec programLoop () =
+    printf "Введите команду: "
+    let input = Console.ReadLine()
+    match input with
+    | "0" -> printHelp (); programLoop ()
+    | "1" -> ()
+    | _ -> printfn "not implemented"; programLoop ()
 
 [<EntryPoint>]
 let main argv =
-    printMenu ()
-    0 // return an integer exit code
+    printHelp ()
+    
+    programLoop ()
+        
+    0
 
