53c29fd360c92934d5a8f24dd0c830770ab11b92
reacheight <reacheight8@gmail.com>
2019-04-25 23:27:11 +0300

    implemented accuracy calculator workflow

---
+++ 4th-semester/Homework7/AccuracyCalculatorWorkflow/Program.fs
@@ -0,0 +1,22 @@
+﻿namespace Homework
+
+module AccuracyCalculatorWorkflow =
+    open System
+    
+    type AccuracyCalculatorBuilder (accuracy : int) =
+        member this.Bind(x : float, f) = f x
+        
+        member this.Return(x : float) = Math.Round(x, accuracy)
+    
+    [<EntryPoint>]
+    let main argv =
+        let rounding = AccuracyCalculatorBuilder
+        let result = rounding 3 {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+        }
+        
+        printfn "Результат вычислений: %f" result
+        
+        0
