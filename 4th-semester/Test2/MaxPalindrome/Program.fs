1ba254699bac6cacc2cb0ee7789dece11ef17cc1
reacheight <reacheight8@gmail.com>
2019-04-26 18:25:10 +0300

    added comments

--- 4th-semester/Test2/MaxPalindrome/Program.fs
+++ 4th-semester/Test2/MaxPalindrome/Program.fs
@@ -1,19 +1,24 @@
 ﻿namespace Test
 
+/// Module for searching for the biggest palindrome that is product of two three-digit numbers
 module MaxPalindrome = 
     open System
     
+    /// Gets whether string is palindrome
     let isPalindrome string =
         let reverse = string |> Seq.rev |> Seq.take (String.length string) |> String.Concat
         string = reverse
     
+    /// Gets cartesian product of two lists
     let cartesianProduct first second =
         first |> List.collect (fun x -> second |> List.map (fun y -> (x, y)))
     
+    /// Finds the biggest palindrome that is product of two three-digit numbers
     let findMaxPalindrome =
-        let allNumbers = seq { 111 .. 999 } |> Seq.toList
-        let allPairs = cartesianProduct allNumbers allNumbers
-        allPairs
+        let threeDigitNumbers = seq { 111 .. 999 } |> Seq.toList
+        let pairs = cartesianProduct threeDigitNumbers threeDigitNumbers
+        
+        pairs
         |> List.map (fun (x, y) -> (x * y).ToString())
         |> List.filter isPalindrome
         |> List.maxBy Int32.Parse
