b26b2d99d49a6e9e282f115b244003a122f3dd1f
reacheight <reacheight8@gmail.com>
2019-04-26 18:11:31 +0300

    implemented priority queue and added some tests

---
+++ 4th-semester/Test2/PriorityQueue/PriorityQueue.fs
@@ -0,0 +1,29 @@
+﻿namespace Test
+
+open System.Collections.Generic
+open System.Linq
+
+/// Mutable priority queue implementation
+type PriorityQueue<'T>() =
+    let mutable queue = new List<'T * int>()
+    
+    /// Gets whether queue is empty
+    member this.IsEmpty
+        with get() = queue.Count = 0
+    
+    /// Enqueues value with weight
+    member this.Enqueue(value, weigth) =
+        let index = queue.FindIndex(fun (v, w) -> w > weigth)
+        match index with
+        | -1 -> queue.Add(value, weigth)
+        | i -> queue.Insert(i, (value, weigth))
+        
+    /// Deques value with max weight
+    member this.Dequeue() =
+        if queue.Count = 0
+            then
+                failwith "Queue is empty!"
+            else
+                let result = queue.Last() |> fst
+                queue.RemoveAt(queue.Count - 1)
+                result
---
+++ 4th-semester/Test2/Test.Tests/QueueTests.fs
@@ -0,0 +1,51 @@
+namespace Test.Tests
+
+open NUnit.Framework
+open FsUnit
+open System
+open Test
+
+[<TestFixture>]
+type ``queue should`` () =
+    let mutable queue = PriorityQueue<int>()
+    
+    [<SetUp>]
+    member this.SetUp() =
+        queue <- new PriorityQueue<int>()
+
+    [<Test>]
+    member this.``be empty after creation`` () =
+        queue.IsEmpty |> should be True
+     
+    [<Test>]
+    member this.``not be empty after enqueue`` () =
+        queue.Enqueue(3, 10)
+        queue.IsEmpty |> should be False
+        
+    [<Test>]
+    member this.``be empty after queue-dequeue`` () =
+        queue.Enqueue(3, 5)
+        queue.Dequeue() |> ignore
+        queue.IsEmpty |> should be True
+    
+    [<Test>]
+    member this.``dequeue enqueued value after first enqueue`` () =
+        queue.Enqueue(213, 5)
+        queue.Dequeue() |> should equal 213
+    
+    [<Test>]
+    member this.``dequeue value with max weight`` () =
+        queue.Enqueue(6, 9)
+        queue.Enqueue(3, 10)
+        queue.Enqueue(7, 4)
+        
+        queue.Dequeue() |> should equal 3
+    
+    [<Test>]
+    member this.``dequeue value with max weight multiple times`` () =
+        queue.Enqueue(7, 9)
+        queue.Enqueue(3, 10)
+        queue.Enqueue(6, 4)
+        
+        queue.Dequeue() |> should equal 3
+        queue.Dequeue() |> should equal 7
