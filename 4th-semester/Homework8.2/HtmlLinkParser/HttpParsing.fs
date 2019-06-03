c113f99b32c88b581adca9dc5670f0d305bf5dd6
reacheight <reacheight8@gmail.com>
2019-06-04 01:13:08 +0300

    http parsing separated from entry point file

---
+++ 4th-semester/Homework8.2/HtmlLinkParser/HttpParsing.fs
@@ -0,0 +1,30 @@
+module HtmlLinkParser.HttpParsing
+
+open FSharp.Collections.ParallelSeq
+open HtmlAgilityPack
+open System
+
+let getAllHttpUrlsFromUrlHtml (url : string) =
+    let htmlParser = HtmlWeb()
+    let htmlDoc = htmlParser.Load(url)
+    htmlDoc.DocumentNode.SelectNodes("//a")
+        |> Seq.map (fun node -> node.Attributes.["href"].Value)
+        |> Seq.filter (fun url -> url.StartsWith("http://"))
+        |> Seq.toList
+
+let getUrlHtmlSize (url : string) =
+    HtmlWeb().Load(url).Text.Length
+  
+let isUrl (candidate : string) =
+    let mutable uri = Uri("http://empty.com")
+    Uri.TryCreate(candidate, UriKind.Absolute,  &uri)
+        && (uri.Scheme = Uri.UriSchemeHttp || uri.Scheme = Uri.UriSchemeHttps);
+   
+let printAllUrlsFromUrlHtml (startUrl : string) =
+    let containedUrls = startUrl |> getAllHttpUrlsFromUrlHtml
+    let urlSizePairs = containedUrls
+                       |> PSeq.map (fun url -> url, getUrlHtmlSize url)
+                       |> PSeq.toList
+    
+    Console.WriteLine("All urls from " + startUrl + ":")
+    urlSizePairs |> Seq.iter (fun (url, size) -> printfn "%s --- %i" url size)
--- 4th-semester/Homework8.2/HtmlLinkParser/Program.fs
+++ 4th-semester/Homework8.2/HtmlLinkParser/Program.fs
@@ -1,31 +1,5 @@
-﻿open FSharp.Collections.ParallelSeq
-open HtmlAgilityPack
-open System
-
-let getAllHttpUrlsFromUrlHtml (url : string) =
-    let htmlParser = HtmlWeb()
-    let htmlDoc = htmlParser.Load(url)
-    htmlDoc.DocumentNode.SelectNodes("//a")
-        |> Seq.map (fun node -> node.Attributes.["href"].Value)
-        |> Seq.filter (fun url -> url.StartsWith("http://"))
-        |> Seq.toList
-
-let getUrlHtmlSize (url : string) =
-    HtmlWeb().Load(url).Text.Length
-  
-let isUrl (candidate : string) =
-    let mutable uri = Uri("http://empty.com")
-    Uri.TryCreate(candidate, UriKind.Absolute,  &uri)
-        && (uri.Scheme = Uri.UriSchemeHttp || uri.Scheme = Uri.UriSchemeHttps);
-   
-let printAllUrlsFromUrlHtml (startUrl : string) =
-    let containedUrls = startUrl |> getAllHttpUrlsFromUrlHtml
-    let urlSizePairs = containedUrls
-                       |> PSeq.map (fun url -> url, getUrlHtmlSize url)
-                       |> PSeq.toList
-    
-    Console.WriteLine("All urls from " + startUrl + ":")
-    urlSizePairs |> Seq.iter (fun (url, size) -> printfn "%s --- %i" url size)
+﻿open System
+open HtmlLinkParser.HttpParsing
 
 [<EntryPoint>]
 let main argv =
