809e3206208312706b80efce21fe496b34814126
reacheight <reacheight8@gmail.com>
2019-06-05 21:17:19 +0300

    added point free task solution

---
+++ 4th-semester/NewHomework5/Pointfree/Pointfree.fs
@@ -0,0 +1,11 @@
+﻿module Pointfree
+
+let multiplyAll x l = List.map (fun y -> y * x) l
+
+let multiplyAll'1 x = List.map (fun y -> y * x)
+
+let multiplyAll'2 x = List.map (fun y -> (*) x y)
+
+let multiplyAll'3 x = List.map ((*) x)
+
+let multiplyAll'4 = List.map << (*)
