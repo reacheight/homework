71ee0d453e9ee4389c84f7ae1ae29a7f7b5eaea6
reacheight <reacheight8@gmail.com>
2019-06-05 16:57:22 +0300

    added simple multi threaded lazy tests

--- 4th-semester/Homework8.1/Lazy.Tests/MultiThreadedLazyTests.fs
+++ 4th-semester/Homework8.1/Lazy.Tests/MultiThreadedLazyTests.fs
@@ -11,4 +11,24 @@ type ``multi threaded lazy should``() =
     
     [<SetUp>]
     member this.SetUp() =
-        lazyObject <- LazyFactory.CreateMultiThreadedLazy (fun () -> "result")
+        lazyObject <- LazyFactory.CreateMultiThreadedLazy (supplier)
+        
+    [<Test>]
+    member this.``get result on first call`` () =
+        lazyObject.Get() |> should equal (supplier ())
+        
+    [<Test>]
+    member this.``get result on multiple calls`` () =
+        for i in 1..10 do
+            lazyObject.Get() |> should equal (supplier ())
+        
+    [<Test>]
+    member this.``call supplier function only once`` () =
+        let mutable evaluationCount = 0
+        lazyObject <- LazyFactory.CreateSingleThreadedLazy
+                          (fun () -> evaluationCount <- evaluationCount + 1
+                                     supplier())
+        for i in 1..10 do
+            lazyObject.Get() |> ignore
+        
+        evaluationCount |> should equal 1
