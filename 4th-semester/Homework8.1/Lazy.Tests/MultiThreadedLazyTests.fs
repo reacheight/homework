a76e2c321c77755cc6ccef1db3f34075464636ef
reacheight <reacheight8@gmail.com>
2019-06-05 17:18:31 +0300

    added a multi threaded lazy test

--- 4th-semester/Homework8.1/Lazy.Tests/MultiThreadedLazyTests.fs
+++ 4th-semester/Homework8.1/Lazy.Tests/MultiThreadedLazyTests.fs
@@ -1,5 +1,6 @@
 namespace Lazy.Tests
 
+open System.Threading
 open NUnit.Framework
 open FsUnit
 open Lazy
@@ -8,6 +9,15 @@ open Lazy
 type ``multi threaded lazy should``() =
     let supplier = fun () -> "result"
     let mutable lazyObject = LazyFactory.CreateMultiThreadedLazy (supplier)
+    
+    let workOnMultipleThreads (supplier : unit -> 'a) =
+        let results = ResizeArray<'a>(10)
+        let threads = List.init results.Count (fun i -> Thread(fun () -> results.[i] <- supplier()))
+        threads |> List.iter (fun thread -> thread.Start())
+        threads |> List.iter (fun thread -> thread.Join())
+        
+        results |> Seq.toList
+        
     
     [<SetUp>]
     member this.SetUp() =
@@ -32,3 +42,8 @@ type ``multi threaded lazy should``() =
             lazyObject.Get() |> ignore
         
         evaluationCount |> should equal 1
+        
+    [<Test>]
+    member this.``get result in multiple threads`` () =
+        let results = workOnMultipleThreads supplier
+        results |> List.forall (fun result -> result = supplier()) |> should be True
