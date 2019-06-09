122794267a52e0b42a6e5b7e73ca30dd1ee3f23c
reacheight <reacheight8@gmail.com>
2019-06-09 11:29:38 +0300

    refactored phonebook module

--- 4th-semester/NewHomework5/Phonebook/Phonebook.fs
+++ 4th-semester/NewHomework5/Phonebook/Phonebook.fs
@@ -2,23 +2,22 @@ module Phonebook
 
 open System
 open System.IO
-open System.Text.RegularExpressions
 
 type Phonebook = Map<string, string>
 
 /// Adds new record to a phonebook
 /// If phonebook contains record with given name, overrides it
-let addRecord (phonebook : Phonebook) name phone : Phonebook=
+let addRecord name phone (phonebook : Phonebook) : Phonebook=
     phonebook |> Map.add name phone
     
 /// Finds phone by name
-let findPhone (phonebook : Phonebook) name =
+let findPhone name (phonebook : Phonebook) =
     if phonebook |> Map.containsKey name
         then Some (phonebook |> Map.find name)
         else None
         
 /// Finds all names by phone
-let findNames (phonebook : Phonebook) phone =
+let findNames phone (phonebook : Phonebook) =
     let phoneRecords = phonebook |> Map.filter (fun key value -> value = phone)
     if Map.isEmpty phoneRecords
         then None
@@ -29,7 +28,7 @@ let getAllRecords (phonebook : Phonebook) =
     phonebook |> Map.toList
 
 /// Saves phonebook data to file
-let saveToFile (phonebook : Phonebook) (path : string) =
+let saveToFile (path : string) (phonebook : Phonebook) =
     use writer = new StreamWriter (path)
     phonebook |> Map.iter (fun key value -> writer.WriteLine(sprintf "%s : %s" key value))
     
@@ -44,3 +43,7 @@ let loadPhonebookFromFile (path : string) : Phonebook =
             then raise (FormatException("File contains string in wrong format."))
         (words.[0].Trim(), words.[1].Trim()))
     |> Map.ofSeq
+
+/// Gets whether phonebook contains record with given name
+let containsName name (phonebook : Phonebook) =
+    phonebook |> Map.containsKey name
