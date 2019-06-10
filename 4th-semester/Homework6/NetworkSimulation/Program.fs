527fb06e050ad966191b5bbbba52a4a746ea66c2
reacheight <reacheight8@gmail.com>
2019-06-10 09:25:01 +0300

    added console example

--- 4th-semester/Homework6/NetworkSimulation/Program.fs
+++ 4th-semester/Homework6/NetworkSimulation/Program.fs
@@ -1,6 +1,46 @@
 ﻿module Main
 
+open NetworkSimulation
+open System
+
+type Linux() =
+    interface IOperatingSysmtem with
+        member this.InfectionProbability = 0.3
+
+type Windows() =
+    interface IOperatingSysmtem with
+        member this.InfectionProbability = 0.7
+
 [<EntryPoint>]
 let main argv =
+    let computers = [0 .. 6]
+                    |> List.map (fun i -> Computer(i,
+                                                   i % 3 = 0,
+                                                   if i % 2 = 0
+                                                       then Linux() :> IOperatingSysmtem
+                                                       else Windows() :> IOperatingSysmtem
+                                                       )
+                    )
+                    
+    let connections = [(computers.[0] :> IComputer, [computers.[1] :> IComputer; computers.[3] :> IComputer]);
+                       (computers.[1] :> IComputer, [computers.[0] :> IComputer; computers.[3] :> IComputer; computers.[6] :> IComputer]);
+                       (computers.[2] :> IComputer, [computers.[4] :> IComputer]);
+                       (computers.[3] :> IComputer, [computers.[0] :> IComputer]);
+                       (computers.[4] :> IComputer, [computers.[2] :> IComputer]);
+                       (computers.[5] :> IComputer, []);
+                       (computers.[6] :> IComputer, [computers.[1] :> IComputer])]
     
+    let network = Network(connections)
+    
+    let mutable loop = true
+    while loop do
+        printfn "Текущее состояние сети:"
+        printfn "%A" network.CurrentState
+        
+        printfn "Введите команду:"
+        printfn "0 -- выход, остальное -- сделать ход"
+        let answer = Console.ReadLine()
+        if answer = "0"
+            then loop <- false
+            else network.MakeTurn()
     0
